8abd6d536477a91c6a00f92dfc9e90d73668069b
romanfedorov100-wq <romanfedorov100@gmail.com>
2026-05-22 05:02:44 +0300

    Update league-data.js

--- league-data.js
+++ league-data.js
@@ -1,523 +1,130 @@
-window.deportivoLeague = {
-  source: "https://ligaescobarense.ar/index.php/competition/primera-division-b-2025-26-zona-escobar/",
-
-  teams: {
-    "Falucho": {
-      logo: "https://ligaescobarense.ar/wp-content/uploads/2024/01/Falucho.png"
-    },
-    "America": {
-      logo: "https://ligaescobarense.ar/wp-content/uploads/2024/01/America.png"
-    },
-    "Los Altos": {
-      logo: "https://ligaescobarense.ar/wp-content/uploads/2024/01/Los-Altos.png"
-    },
-    "Domingo Matheu": {
-      logo: "https://ligaescobarense.ar/wp-content/uploads/2024/01/Domingo-Matheu.png"
-    },
-    "Deportivo Moscu": {
-      logo: "images/logo.png"
-    },
-    "Lomas Futbol": {
-      logo: "https://ligaescobarense.ar/wp-content/uploads/2024/12/Lomas-Futbol.png"
-    },
-    "Fair Play": {
-      logo: "https://ligaescobarense.ar/wp-content/uploads/2025/10/Fair-Play.png"
-    },
-    "Villa Juana": {
-      logo: "https://ligaescobarense.ar/wp-content/uploads/2024/01/Villa-Juana.png"
-    },
-    "Colectividad Boliviana": {
-      logo: "https://ligaescobarense.ar/wp-content/uploads/2024/01/C.-B.-E.png"
-    },
-    "Capitanes": {
-      logo: "https://ligaescobarense.ar/wp-content/uploads/2024/01/Capitanes.png"
-    },
-    "Parque III": {
-      logo: "https://ligaescobarense.ar/wp-content/uploads/2024/01/Parque-Villarino-1.png"
-    },
-    "La Sonia B": {
-      logo: "https://ligaescobarense.ar/wp-content/uploads/2024/08/La-Sonia-nuevo.png"
-    },
-    "Tribuna Sport Club": {
-      logo: "https://ligaescobarense.ar/wp-content/uploads/2025/10/Tribuna-Sport-Club.png"
-    },
-    "Sportivo Union": {
-      logo: "https://ligaescobarense.ar/wp-content/uploads/2024/09/Sportivo-Union.png"
-    }
+window.leagueTable = [
+  {
+    team: "Falucho",
+    played: 21,
+    wins: 18,
+    draws: 0,
+    losses: 3,
+    goals: "64:20",
+    points: 54
   },
+  {
+    team: "America",
+    played: 21,
+    wins: 14,
+    draws: 1,
+    losses: 6,
+    goals: "68:41",
+    points: 43
+  },
+  {
+    team: "Los Altos",
+    played: 21,
+    wins: 13,
+    draws: 2,
+    losses: 6,
+    goals: "48:32",
+    points: 41
+  },
+  {
+    team: "Domingo Matheu",
+    played: 20,
+    wins: 12,
+    draws: 3,
+    losses: 5,
+    goals: "41:30",
+    points: 39
+  },
+  {
+    team: "Deportivo Moscu",
+    played: 21,
+    wins: 12,
+    draws: 2,
+    losses: 7,
+    goals: "44:38",
+    points: 38
+  },
+  {
+    team: "Lomas Futbol",
+    played: 20,
+    wins: 10,
+    draws: 4,
+    losses: 6,
+    goals: "41:25",
+    points: 34
+  },
+  {
+    team: "Fair Play",
+    played: 21,
+    wins: 9,
+    draws: 5,
+    losses: 7,
+    goals: "41:34",
+    points: 32
+  },
+  {
+    team: "Villa Juana",
+    played: 21,
+    wins: 8,
+    draws: 3,
+    losses: 10,
+    goals: "37:40",
+    points: 27
+  },
+  {
+    team: "Colectividad Boliviana",
+    played: 21,
+    wins: 7,
+    draws: 3,
+    losses: 11,
+    goals: "51:56",
+    points: 24
+  },
+  {
+    team: "Capitanes",
+    played: 21,
+    wins: 5,
+    draws: 7,
+    losses: 9,
+    goals: "36:48",
+    points: 22
+  },
+  {
+    team: "Tribuna Sport Club",
+    played: 21,
+    wins: 3,
+    draws: 7,
+    losses: 11,
+    goals: "21:44",
+    points: 16
+  },
+  {
+    team: "Parque III",
+    played: 21,
+    wins: 5,
+    draws: 1,
+    losses: 15,
+    goals: "25:54",
+    points: 16
+  },
+  {
+    team: "La Sonia B",
+    played: 21,
+    wins: 4,
+    draws: 4,
+    losses: 13,
+    goals: "22:55",
+    points: 16
+  },
+  {
+    team: "Sportivo Union",
+    played: 21,
+    wins: 4,
+    draws: 2,
+    losses: 15,
+    goals: "37:59",
+    points: 14
+  }
+];
 
-  table: [
-    {
-      position: 1,
-      team: "Falucho",
-      pj: 21,
-      g: 18,
-      e: 0,
-      p: 3,
-      gf: 64,
-      gc: 20,
-      dif: 44,
-      pts: 54
-    },
-    {
-      position: 2,
-      team: "America",
-      pj: 21,
-      g: 14,
-      e: 1,
-      p: 6,
-      gf: 68,
-      gc: 41,
-      dif: 27,
-      pts: 43
-    },
-    {
-      position: 3,
-      team: "Los Altos",
-      pj: 21,
-      g: 13,
-      e: 2,
-      p: 6,
-      gf: 48,
-      gc: 32,
-      dif: 16,
-      pts: 41
-    },
-    {
-      position: 4,
-      team: "Domingo Matheu",
-      pj: 20,
-      g: 12,
-      e: 3,
-      p: 5,
-      gf: 41,
-      gc: 30,
-      dif: 11,
-      pts: 39
-    },
-    {
-      position: 5,
-      team: "Deportivo Moscu",
-      pj: 21,
-      g: 12,
-      e: 2,
-      p: 7,
-      gf: 44,
-      gc: 38,
-      dif: 6,
-      pts: 38
-    },
-    {
-      position: 6,
-      team: "Lomas Futbol",
-      pj: 20,
-      g: 10,
-      e: 4,
-      p: 6,
-      gf: 41,
-      gc: 25,
-      dif: 16,
-      pts: 34
-    },
-    {
-      position: 7,
-      team: "Fair Play",
-      pj: 21,
-      g: 9,
-      e: 5,
-      p: 7,
-      gf: 41,
-      gc: 34,
-      dif: 7,
-      pts: 32
-    },
-    {
-      position: 8,
-      team: "Villa Juana",
-      pj: 21,
-      g: 8,
-      e: 3,
-      p: 10,
-      gf: 37,
-      gc: 40,
-      dif: -3,
-      pts: 27
-    },
-    {
-      position: 9,
-      team: "Colectividad Boliviana",
-      pj: 21,
-      g: 7,
-      e: 3,
-      p: 11,
-      gf: 51,
-      gc: 56,
-      dif: -5,
-      pts: 24
-    },
-    {
-      position: 10,
-      team: "Capitanes",
-      pj: 20,
-      g: 5,
-      e: 6,
-      p: 9,
-      gf: 33,
-      gc: 45,
-      dif: -12,
-      pts: 21
-    },
-    {
-      position: 11,
-      team: "Parque III",
-      pj: 21,
-      g: 5,
-      e: 1,
-      p: 15,
-      gf: 25,
-      gc: 54,
-      dif: -29,
-      pts: 16
-    },
-    {
-      position: 12,
-      team: "La Sonia B",
-      pj: 21,
-      g: 4,
-      e: 4,
-      p: 13,
-      gf: 22,
-      gc: 55,
-      dif: -33,
-      pts: 16
-    },
-    {
-      position: 13,
-      team: "Tribuna Sport Club",
-      pj: 20,
-      g: 3,
-      e: 6,
-      p: 11,
-      gf: 18,
-      gc: 41,
-      dif: -23,
-      pts: 15
-    },
-    {
-      position: 14,
-      team: "Sportivo Union",
-      pj: 21,
-      g: 4,
-      e: 2,
-      p: 15,
-      gf: 37,
-      gc: 59,
-      dif: -22,
-      pts: 14
-    }
-  ],
-
-  matches: [
-    {
-      round: "Fecha 1",
-      date: "1 Nov 2025",
-      home: "Domingo Matheu",
-      away: "La Sonia B",
-      homeScore: 2,
-      awayScore: 0
-    },
-    {
-      round: "Fecha 1",
-      date: "1 Nov 2025",
-      home: "Colectividad Boliviana",
-      away: "America",
-      homeScore: 1,
-      awayScore: 5
-    },
-    {
-      round: "Fecha 1",
-      date: "1 Nov 2025",
-      home: "Falucho",
-      away: "Villa Juana",
-      homeScore: 2,
-      awayScore: 1
-    },
-    {
-      round: "Fecha 1",
-      date: "2 Nov 2025",
-      home: "Sportivo Union",
-      away: "Tribuna Sport Club",
-      homeScore: 2,
-      awayScore: 2
-    },
-    {
-      round: "Fecha 1",
-      date: "2 Nov 2025",
-      home: "Los Altos",
-      away: "Fair Play",
-      homeScore: 0,
-      awayScore: 2
-    },
-    {
-      round: "Fecha 1",
-      date: "2 Nov 2025",
-      home: "Deportivo Moscu",
-      away: "Capitanes",
-      homeScore: 0,
-      awayScore: 1
-    },
-    {
-      round: "Fecha 1",
-      date: "2 Nov 2025",
-      home: "Parque III",
-      away: "Lomas Futbol",
-      homeScore: 0,
-      awayScore: 3
-    },
-
-    {
-      round: "Fecha 2",
-      date: "8 Nov 2025",
-      home: "Villa Juana",
-      away: "Colectividad Boliviana",
-      homeScore: 1,
-      awayScore: 2
-    },
-    {
-      round: "Fecha 2",
-      date: "8 Nov 2025",
-      home: "America",
-      away: "Deportivo Moscu",
-      homeScore: 1,
-      awayScore: 0
-    },
-    {
-      round: "Fecha 2",
-      date: "9 Nov 2025",
-      home: "Sportivo Union",
-      away: "Los Altos",
-      homeScore: 0,
-      awayScore: 4
-    },
-    {
-      round: "Fecha 2",
-      date: "9 Nov 2025",
-      home: "Tribuna Sport Club",
-      away: "Parque III",
-      homeScore: 2,
-      awayScore: 3
-    },
-    {
-      round: "Fecha 2",
-      date: "9 Nov 2025",
-      home: "Lomas Futbol",
-      away: "Domingo Matheu",
-      homeScore: 0,
-      awayScore: 2
-    },
-    {
-      round: "Fecha 2",
-      date: "9 Nov 2025",
-      home: "Capitanes",
-      away: "Fair Play",
-      homeScore: 5,
-      awayScore: 2
-    },
-    {
-      round: "Fecha 2",
-      date: "10 Nov 2025",
-      home: "La Sonia B",
-      away: "Falucho",
-      homeScore: 0,
-      awayScore: 7
-    },
-
-    {
-      round: "Fecha 3",
-      date: "15 Nov 2025",
-      home: "Fair Play",
-      away: "America",
-      homeScore: 1,
-      awayScore: 3
-    },
-    {
-      round: "Fecha 3",
-      date: "15 Nov 2025",
-      home: "Colectividad Boliviana",
-      away: "La Sonia B",
-      homeScore: 2,
-      awayScore: 2
-    },
-    {
-      round: "Fecha 3",
-      date: "15 Nov 2025",
-      home: "Falucho",
-      away: "Lomas Futbol",
-      homeScore: 1,
-      awayScore: 2
-    },
-    {
-      round: "Fecha 3",
-      date: "15 Nov 2025",
-      home: "Domingo Matheu",
-      away: "Tribuna Sport Club",
-      homeScore: 4,
-      awayScore: 1
-    },
-    {
-      round: "Fecha 3",
-      date: "15 Nov 2025",
-      home: "Parque III",
-      away: "Sportivo Union",
-      homeScore: 3,
-      awayScore: 1
-    },
-    {
-      round: "Fecha 3",
-      date: "16 Nov 2025",
-      home: "Deportivo Moscu",
-      away: "Villa Juana",
-      homeScore: 5,
-      awayScore: 1
-    },
-    {
-      round: "Fecha 3",
-      date: "22 Nov 2025",
-      home: "Los Altos",
-      away: "Capitanes",
-      homeScore: 4,
-      awayScore: 1
-    },
-
-    {
-      round: "Fecha 4",
-      date: "22 Nov 2025",
-      home: "Lomas Futbol",
-      away: "Colectividad Boliviana",
-      homeScore: 1,
-      awayScore: 2
-    },
-    {
-      round: "Fecha 4",
-      date: "22 Nov 2025",
-      home: "Villa Juana",
-      away: "Fair Play",
-      homeScore: 3,
-      awayScore: 1
-    },
-    {
-      round: "Fecha 4",
-      date: "23 Nov 2025",
-      home: "Tribuna Sport Club",
-      away: "Falucho",
-      homeScore: 0,
-      awayScore: 3
-    },
-    {
-      round: "Fecha 4",
-      date: "24 Nov 2025",
-      home: "Parque III",
-      away: "Los Altos",
-      homeScore: 2,
-      awayScore: 3
-    },
-    {
-      round: "Fecha 4",
-      date: "24 Nov 2025",
-      home: "Sportivo Union",
-      away: "Domingo Matheu",
-      homeScore: 1,
-      awayScore: 2
-    },
-    {
-      round: "Fecha 4",
-      date: "24 Nov 2025",
-      home: "La Sonia B",
-      away: "Deportivo Moscu",
-      homeScore: 0,
-      awayScore: 1
-    },
-    {
-      round: "Fecha 4",
-      date: "24 Nov 2025",
-      home: "America",
-      away: "Capitanes",
-      homeScore: 2,
-      awayScore: 1
-    },
-
-    {
-      round: "Fecha 5",
-      date: "30 Nov 2025",
-      home: "Deportivo Moscu",
-      away: "Lomas Futbol",
-      homeScore: 1,
-      awayScore: 0
-    },
-    {
-      round: "Fecha 5",
-      date: "6 Dic 2025",
-      home: "Capitanes",
-      away: "Villa Juana",
-      homeScore: 3,
-      awayScore: 1
-    },
-    {
-      round: "Fecha 5",
-      date: "6 Dic 2025",
-      home: "Colectividad Boliviana",
-      away: "Tribuna Sport Club",
-      homeScore: 3,
-      awayScore: 0
-    },
-    {
-      round: "Fecha 5",
-      date: "6 Dic 2025",
-      home: "Domingo Matheu",
-      away: "Parque III",
-      homeScore: 3,
-      awayScore: 1
-    },
-    {
-      round: "Fecha 5",
-      date: "7 Dic 2025",
-      home: "Los Altos",
-      away: "America",
-      homeScore: 5,
-      awayScore: 3
-    },
-    {
-      round: "Fecha 5",
-      date: "8 Dic 2025",
-      home: "Fair Play",
-      away: "La Sonia B",
-      homeScore: 7,
-      awayScore: 0
-    },
-    {
-      round: "Fecha 5",
-      date: "8 Dic 2025",
-      home: "Falucho",
-      away: "Sportivo Union",
-      homeScore: 2,
-      awayScore: 1
-    },
-
-    {
-      round: "Fecha 6",
-      date: "8 Dic 2025",
-      home: "Tribuna Sport Club",
-      away: "Deportivo Moscu",
-      homeScore: 1,
-      awayScore: 3
-    },
-    {
-      round: "Fecha 8",
-      date: "18 Ene 2026",
-      home: "Parque III",
-      away: "Deportivo Moscu",
-      homeScore: 1,
-      awayScore: 2
-    }
-  ]
-};
+window.deportivoLeagueTable = window.leagueTable;
